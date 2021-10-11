2165d6fcc33f0716056483b04f7409e3faea7a81
acselerai <differencialx+geekbrains@gmail.com>
2021-10-11 20:27:20 +0300

    Add average salary for task_3

--- home_work_5/task_3.py
+++ home_work_5/task_3.py
@@ -7,3 +7,7 @@ with open('task_3.txt', 'r') as file:
     print('Employees with salary under 20000: ')
     for (employee, salary) in employees_salary.items():
         print(f'{employee}: {salary}')
+
+    salaries = [float(salary) for salary in employees_salary.values()]
+    average_salary = sum(salaries) / len(employees_salary)
+    print(f'Average salary: {average_salary:.2f}')
